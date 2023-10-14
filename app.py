b37036361c0fe425d5a7e55a77ea2d520ce144dc
farcryson <o.varshith5454@gmail.com>
2023-10-14 17:58:37 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,44 @@
+import streamlit as st
+import tensorflow as tf
+import cv2
+from PIL import Image, ImageOps
+import numpy as np
+
+@st.cache(allow_output_mutation = True)
+def load_model():
+    model = tf.keras.models.load_model('model.h5')
+    return model
+with st.spinner('Model is being loaded...'):
+    model = load_model()
+st.write("""
+        #Alzheimer's Classification
+        """
+        )
+file = st.file_uploader("Upload the image", type = ["jpg", "png"])
+st.set_option('deprecation.showfileUploaderEncoding', False)
+
+def upload_predict(upload_image, model):
+    size = (180, 180)
+    image = ImageOps.fit(upload_image, size, Image.ANTIALIAS)
+    image = np.asarray(image)
+    imgage = cv2.resize(image, dsize = (32, 32, 3))
+    
+    prediction = model.predict(image)
+    ans = np.argmax(prediction, axis = 1)
+    return ans
+if file is None:
+    st.text("Please upload an image file")
+else:
+    image  = Image.open(file)
+    st.image(image, use_column_width = True)
+    predictions = upload_predict(image, model)
+    if str(predictions[0] == 0):
+        image_class = "MildDemented"
+    elif str(predictions[0] == 0):
+        image_class = "ModerateDemented"
+    elif str(predictions[0] == 0):
+        image_class = "NonDemented"
+    else:
+        image_class = "VeryMildDemented"
+    st.write("The image is classified as ", image_class)
+    
