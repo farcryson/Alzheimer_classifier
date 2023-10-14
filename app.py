b9db8d1a19cdb8afea9adee3d00a8d2aa67c1299
farcryson <o.varshith5454@gmail.com>
2023-10-14 20:05:39 +0530

    changes

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import cv2
 from PIL import Image, ImageOps
 import numpy as np
 
-@st.cache_data(allow_output_mutation = True)
+@st.cache_data()
 def load_model():
     model = tf.keras.models.load_model('model.hdf5')
     return model
@@ -14,28 +14,27 @@ st.title("Alzheimer's Classification")
 file = st.file_uploader("Upload the image", type = ["jpg", "png"])
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-def upload_predict(upload_image, model):
-    size = (180, 180)
-    image = ImageOps.fit(upload_image, size)
-    image = np.asarray(image)
-    imgage = cv2.resize(image, dsize = (32, 32, 3))
-    
-    prediction = model.predict(image)
+def upload_predict(img, model):
+    test_image = tf.keras.preprocessing.image.load_img(img, target_size=(32, 32))
+    test_image = tf.keras.preprocessing.image.img_to_array(test_image)
+    test_image = np.expand_dims(test_image, axis = 0)
+    prediction = model.predict(test_image)
     ans = np.argmax(prediction, axis = 1)
     return ans
+
 if file is None:
     st.text("Please upload an image file")
 else:
-    image  = Image.open(file)
-    st.image(image, use_column_width = True)
-    predictions = upload_predict(image, model)
-    if str(predictions[0] == 0):
+    prediction = upload_predict(file, model)
+    img = tf.keras.preprocessing.image.load_img(file)
+    st.image(img, width = 300)
+    if str(prediction[0] == 0):
         image_class = "MildDemented"
-    elif str(predictions[0] == 0):
+    elif str(prediction[0] == 0):
         image_class = "ModerateDemented"
-    elif str(predictions[0] == 0):
+    elif str(prediction[0] == 0):
         image_class = "NonDemented"
     else:
         image_class = "VeryMildDemented"
-    st.write("The image is classified as ", image_class)
+    st.write("This image is classified as ", image_class)
     
